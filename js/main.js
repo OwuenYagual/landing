97337b98f752ece009dbac16a1998f6c32bac9ec
Owen Yagual Panimboza <oayagual@espol.edu.ec>
2024-11-30 20:12:29 -0500

    logica de negocio terminada

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 const databaseURL = "https://landing-9bcd0-default-rtdb.firebaseio.com/collection.json"
 
+const eventTypeMap = {
+    1: "Quinceañera",
+    2: "Recepción de boda",
+    3: "Graduación",
+    4: "Competición",
+    5: "Cumpleaños"
+};
+
 let sendData = () => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
+    const eventTypeValue = data.event_type;
+    data.event_type = eventTypeMap[eventTypeValue];
     console.log(data)
 
-    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+    
+
+    //data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
 
     fetch(databaseURL, {
         method: 'POST',
@@ -26,7 +38,7 @@ let sendData = () => {
             getData()
         })
         .catch(error => {
-            alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+            alert('Hemos experimentado un error666. ¡Vuelve pronto!'); // Maneja el error con un mensaje
         });
 }
 
@@ -41,51 +53,48 @@ let getData = async () => {
         const data = await response.json()
 
         if (data != null) {
-            let countSuscribers = new Map()
+            let reservas = []
 
             if (Object.keys(data).length > 0) {
                 for (let key in data) {
-                    let { email, saved } = data[key]
-                    let date = saved.split(",")[0]
-                    let count = countSuscribers.get(date) || 0;
-                    countSuscribers.set(date, count + 1)
+                    reservas.push(data[key])
                 }
             }
 
-            if (countSuscribers.size > 0) {
+            if (reservas.length > 0) {
                 subscribers.innerHTML = ''
-    
-                for (let [date, count] of countSuscribers) {
+
+                reservas.forEach((reserva, index) => {
+                    let { date, email, event_type, name} = reserva;
                     let rowTemplate = `
                              <tr>
-                                 <th scope="row">1</th>
+                                 <th scope="row">${index +1}</th>
+                                 <td>${event_type}</td>
                                  <td>${date}</td>
-                                 <td>${count}</td>
                              </tr>`
                     subscribers.innerHTML += rowTemplate
-                }
+                    
+                });
+    
             }
         }
 
     
     } catch (error) {
         console.error('Error en la solicitud:', error);
-        alert('Hemos experimentado un error 69. ¡Vuelve pronto!');
+        alert('Hemos experimentado un error8. ¡Vuelve pronto!');
     }
 }
 
 let ready = () => {
     console.log('DOM esta listo')
-    //debugger
     getData();
 }
 
 let loaded = () => {
     let myform = document.getElementById('form')
-    //debugger
     myform.addEventListener('submit', (eventSubmit) => {
         eventSubmit.preventDefault();
-        //debugger
         let emailElement = document.querySelector('.form-control-lg')
         let emailText = emailElement.value
 
